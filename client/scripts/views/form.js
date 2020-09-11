0cd44567e427b92548ac4c3884b48a32c2f5a5ec
Niaz Morshed Nayeem <36382069+Nayeem-XTREME@users.noreply.github.com>
2020-09-11 11:40:13 +0600

    Form data submit event

--- client/scripts/views/form.js
+++ client/scripts/views/form.js
@@ -2,11 +2,13 @@
  * Created by sohel on 3/30/15.
  */
 
+// let fs = require('fs');
+
 define([
     'text!template/form.html', 'underscore'
   ], function (FormTemplate, _) {
     'use strict';
-  
+
     var Form = Backbone.View.extend({
   
       el: '#page-content',
@@ -19,14 +21,46 @@ define([
   
       className: '',
   
-      events: {},
+      events: {
+        "submit": "save"
+      },
   
       initialize: function () {
+        _.bindAll(this, 'save');
       },
   
       render: function () {
         this.$el.html(this.template());
+      },
+
+      save: function (e) {
+        e.preventDefault();
+
+        let data = {
+          name: $('#name').val(),
+          email: $('#email').val(),
+          address: $('#address').val(),
+          mobile: $('#mobile').val(),
+          book: $('#book').val(),
+          author: $('#author').val(),
+          borrowDate: $('#borrow').val(),
+          returnDate: $('#return').val()
+        }
+
+        alert(JSON.stringify(data));
+
+        // let values = fs.readFileSync('./server/models/info.json').toString();
+        // let users = JSON.parse(values);
+        // users.push(data);
+
+        // fs.writeFileSync('./server/models/info.json', users);
+
+        $('#myForm')[0].reset();
+
       }
+
+
+
     });
     return Form;
   });
